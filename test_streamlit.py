03ee1b0ec18f5239fe00574c0d2b3965f7db6fb0
B_K <60977054+l3oatkunGG@users.noreply.github.com>
2023-04-29 18:23:39 +0700

    add test streamlit app

---
+++ test_streamlit.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+model = tf.saved_model.load("rock_resnet_model")
+
+st.title("Rock Classification")
+
+img_file = st.file_uploader("รูปภาพ", type=["png", "jpg", "jpeg"])
+if img_file is not None:
+    img = keras.utils.load_img(img_file, target_size=(224, 224))
+    input_array = keras.utils.img_to_array(img)
+
+    prediction, label = model(input_array)
+
+    label = [i.numpy().decode("utf-8") for i in label]
+
+    st.image(img_file)
+    
+    fig, ax = plt.subplots()
+    ax.barh(label, prediction.numpy().reshape((4,)))
+    st.pyplot(fig)
+    
