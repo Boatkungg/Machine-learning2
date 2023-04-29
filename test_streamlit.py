e71b39db1d7d5edc5bc49a511041dacf509e0acb
B_K <60977054+l3oatkunGG@users.noreply.github.com>
2023-04-29 20:10:21 +0700

    update streamlit

--- test_streamlit.py
+++ test_streamlit.py
@@ -3,7 +3,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras
 
-model = tf.saved_model.load("rock_resnet_model")
+@st.cache_resource
+def load_model():
+    return tf.saved_model.load("rock_resnet_model")
+
+with st.spinner("กำลังเริ่มต้น"):
+    model = load_model()
 
 st.title("Rock Classification")
 
@@ -12,13 +17,37 @@ if img_file is not None:
     img = keras.utils.load_img(img_file, target_size=(224, 224))
     input_array = keras.utils.img_to_array(img)
 
-    prediction, label = model(input_array)
+    with st.spinner("กำลังประมวณผล"):
+        prediction, label = model(input_array)
 
     label = [i.numpy().decode("utf-8") for i in label]
 
-    st.image(img_file, caption=label[prediction.numpy().argmax()])
+    max_class = prediction.numpy().argmax()
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(img_file, caption=label[max_class])
     
-    fig, ax = plt.subplots()
-    ax.barh(label, prediction.numpy().reshape((4,)))
-    st.pyplot(fig)
+    with col2:
+        fig, ax = plt.subplots()
+        ax.barh(label, prediction.numpy().reshape((4,)))
+        st.pyplot(fig)
+
+    percent = round(prediction.numpy().reshape((4,))[max_class] * 100, 2)
+
+    color = "purple"
+    if percent >= 80:
+        color = "lime"
+    elif percent >= 60:
+        color = "green"
+    elif percent >= 40:
+        color = "orange"
+    elif percent >= 20:
+        color = "red"
+    else:
+        color = "purple"
+
+    st.markdown(f'### {label[max_class]} การประเมินผล <span style="color:{color}"> {percent}% </span>', unsafe_allow_html=True)
+
+
     
