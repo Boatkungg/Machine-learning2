5e2b901b44efe93bacf4b4b88d9e7581c703c253
TK17250 <TXK285@gmail.com>
2023-05-19 10:18:46 +0700

    Edit (wip)

--- test_streamlit.py
+++ test_streamlit.py
@@ -12,8 +12,13 @@ with st.spinner("กำลังเริ่มต้น"):
 
 st.title("Rock Classification")
 
+cam_mode = st.checkbox("")
+
+cam = st.camera_input("Take a picture")
 img_file = st.file_uploader("โปรดใส่รูปภาพ", type=["png", "jpg", "jpeg"])
-if img_file is not None:
+if img_file is not None or cam is not None:
+    if cam is not None:
+        img_file = cam
     img = keras.utils.load_img(img_file, target_size=(384, 384))
     input_array = keras.utils.img_to_array(img)
 
@@ -48,3 +53,20 @@ if img_file is not None:
         color = "purple"
 
     st.markdown(f'### ผลลัพธ์ {label[max_class]} ความเป็นไปได้ <span style="color:{color}"> {percent}% </span>', unsafe_allow_html=True)
+    
+    rock_type = label[max_class]
+
+    # หินดินดาน
+    if rock_type == "shale":
+        st.markdown("""
+        #### ข้อมูล :
+        
+        * ชื่อ : หินดินดาน (Shale)
+
+        * แหล่งที่มา : โดยทั่วไปแหล่งผลิตหินดินดานส่วนใหญ่จะอยู่ใกล้กับโรงงาน ปูนซีเมนต์
+
+        * แหล่งผลิตหินดินดานแหล่งใหญ่ คือ จังหวัดสระบุรี รองลงมา คือ จังหวัดนครศรีธรรมราช และลำปาง
+
+        * ประโยชน์ : ใช้เป็นวัตถุดิบในอุตสาหกรรมปูนซีเมนต์
+        """)
+        
